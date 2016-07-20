9f7d81ddd23c074fa5c43281eafe9d8a96109ab3
Keith McKellar <keith.mckellar@dpizzurros-mbp.manaproducts.com>
2016-07-20 17:09:03 -0400

    footer updates

--- footer.php
+++ footer.php
@@ -16,16 +16,18 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 
+			<div class="social">
+			<!-- <?php wp_nav_menu( array( 'menu' => 'social', 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?> -->
 
 
+				<a class="social_icon" href=""><img  src="<?php bloginfo('template_directory'); ?>/img/twitter.png" /></a><a class="social_icon" href=""><img  src="<?php bloginfo('template_directory'); ?>/img/instagram.png" /></a><a class="social_icon" href=""><img  src="<?php bloginfo('template_directory'); ?>/img/facebook.png" /></a><a class="social_icon" href=""><img  src="<?php bloginfo('template_directory'); ?>/img/youtube.png" /></a>
+			</div>
 
-			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'styleedit' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'styleedit' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'styleedit' ), 'styleedit', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?> -->
+			<div class="">
+				<p class="tagline_footer"><?php bloginfo('description'); ?></p>
+				<p class="copyright">&copy;<?php echo date("Y") ?> <?php bloginfo('name'); ?></p>
+			</div>
 
-
-
-			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
