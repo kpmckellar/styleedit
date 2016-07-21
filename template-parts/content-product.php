c88a3f1592e461e30a5b7ca3f7802c34db334046
Stefan Dindyal <stefan.dindyal@Manaproducts.com>
2016-07-21 18:02:12 -0400

    prepared single.php file as well as added page template for product page

---
+++ template-parts/content-product.php
@@ -0,0 +1,11 @@
+<?php
+
+	/* Template for displaying single product */
+
+?>
+
+<section class="single-product">
+	
+
+
+</section><!-- .no-results -->
---
+++ template-parts/page-product.php
@@ -0,0 +1,103 @@
+<?php
+
+	/* Template Name: Product */
+
+	get_header();
+
+?>
+
+	<!-- Page content START -->
+	
+	<link href="<?php bloginfo('template_directory'); ?>/css/style-product.css" rel="stylesheet" type="text/css">
+
+	<?php
+		
+		while ( have_posts() ) : the_post();
+			$id = get_the_ID();
+			// desktop image
+			$desktop_img = wp_get_attachment_image_src( get_post_thumbnail_id($id), 'full' );
+			$desktop_imgSrc = $desktop_img[0];
+			// tablet image
+			$tablet_img = wp_get_attachment_image_src( get_post_meta($id, 'page_tablet_img', true), 'full' );
+			$tablet_imgSrc = $tablet_img[0];
+			// mobile image
+			$mobile_img = wp_get_attachment_image_src( get_post_meta($id, 'page_mobile_img', true), 'full' );
+			$mobile_imgSrc = $mobile_img[0];
+
+	?>
+
+		<section id="products">
+			
+			<div class="billboard">
+				
+				<style type="text/css" media="screen">
+					.billboard {
+						background-image: url(<?php echo $mobile_imgSrc; ?>);
+					}
+					@media only screen and (min-width: 481px) {
+						.billboard {
+							background-image: url(<?php echo $desktop_imgSrc; ?>);
+						}	
+					}
+					@media only screen and (min-width: 1025px) {
+						.billboard {
+							background-image: url(<?php echo $desktop_imgSrc; ?>);
+						}	
+					}
+				</style>
+
+				<div id="logo">
+					<h1>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>">Style Edit</a>
+					</h1>
+				</div>
+
+			</div>
+
+			<div class="copy">
+				
+				<h2 class="heading"><?php the_title(); ?>:</h2>
+	
+				<?php
+
+					$product_arguments = array( 'post_type' => 'products', 'orderby' => 'date', 'order' => 'DESC', 'posts_per_page' => -1 );
+					$queue_product = new WP_Query( $product_arguments );
+					
+					if ( $queue_product->have_posts() ) :
+						while ( $queue_product->have_posts() ) : $queue_product->the_post(); 
+							$id = get_the_ID();				
+
+				?>
+
+					<div class="product">
+						
+						<h3><?php the_title(); ?></h3>
+
+					</div>
+
+				<?php 
+		
+						endwhile;
+					endif; 
+					
+					wp_reset_postdata();
+
+				?>
+
+			</div>
+
+		</section>
+
+	<?php 
+
+		endwhile;
+
+	?>	
+
+	<!-- Page content END -->
+
+<?php 
+
+	get_footer();
+
+?>
