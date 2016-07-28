c1b956c392e374f99758cdf808c6db73ad76d118
Keith McKellar <keith.mckellar@dpizzurros-mbp.manaproducts.com>
2016-07-28 10:34:04 -0400

    global js now loads after jquery, fixing broken mobile nav. Commented out navigation.js which we are not currently using but is loading on page

--- functions.php
+++ functions.php
@@ -107,15 +107,18 @@ function styleedit_scripts() {
 
 	// load wordpress defaults
 	wp_enqueue_style( 'styleedit-style', get_stylesheet_uri() );
-	wp_enqueue_script( 'styleedit-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	
+	// Removed by KM. Not currently using
+	// wp_enqueue_script( 'styleedit-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 	wp_enqueue_script( 'styleedit-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}		
 
 	// load global
-	wp_enqueue_script( 'styleedit-scripts-global', $templateURI . '/js/global-scripts.js', array(), '1.0', true );	
+
 	wp_enqueue_script( 'styleedit-scripts-jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', array(), '2.2.4', true );
+	wp_enqueue_script( 'styleedit-scripts-global', $templateURI . '/js/global-scripts.js', array(), '1.0', true );
 
 	// load owl carousel on home and products single page
 	if(is_home() || is_singular('products')){
