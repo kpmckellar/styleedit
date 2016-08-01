b11166b1ff4148cb78e1a4331c989c7f957adb30
Keith McKellar <kpmckellar@gmail.com>
2016-08-01 15:37:42 -0400

    added google analytics to header

--- header.php
+++ header.php
@@ -10,6 +10,16 @@
 <!-- Favicon -->
 <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/favicon.ico" type="image/x-icon">
 <?php wp_head(); ?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-69291081-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="site">
