09fe97f3cff4c1a77b35c101603ac125e06e4961
Keith McKellar <kpmckellar@gmail.com>
2016-08-03 10:16:09 -0400

    added the Rakuten tracking pixels to the footer

--- footer.php
+++ footer.php
@@ -24,5 +24,38 @@
 
 <?php wp_footer(); ?>
 
+<script type=text/javascript src="https://services.xg4ken.com/js/kenshoo.js?cid=f15b3992-46df-44e1-a2f9-a3191e71b7bc" ></script>
+<script>
+	$('#spa').click(function() {
+		kenshoo.trackConversion('113','f15b3992-46df-44e1-a2f9-a3191e71b7bc',{
+		   //OPTIONAL PARAMETERS. FILL VALUES OR REMOVE UNNEEDED PARAMETERS
+		   conversionType: 'spa', //specific conversion type. example: type:'AppInstall' default is 'conv'
+		   revenue: 0.0, //numeric conversion value. example convValue: 12.34
+		   currency:'USD', //example currency:'USD'
+		   orderId:'',//example orderId: 'abc'
+		   promoCode:'',
+		   customParam1:'', //any custom parameter. example: Airport: 'JFK'
+		   customParam2:'', //any custom parameter. example: Rooms: '3'
+		   customParamN:'' })
+		})
+
+	$('#locate').click(function() {
+		kenshoo.trackConversion('113','f15b3992-46df-44e1-a2f9-a3191e71b7bc',{
+		   //OPTIONAL PARAMETERS. FILL VALUES OR REMOVE UNNEEDED PARAMETERS
+		   conversionType: 'locate', //specific conversion type. example: type:'AppInstall' default is 'conv'
+		   revenue: 0.0, //numeric conversion value. example convValue: 12.34
+		   currency:'USD', //example currency:'USD'
+		   orderId:'',//example orderId: 'abc'
+		   promoCode:'',
+		   customParam1:'', //any custom parameter. example: Airport: 'JFK'
+		   customParam2:'', //any custom parameter. example: Rooms: '3'
+		   customParamN:'' })
+	})
+</script>
+
+<noscript>
+   <img src="https://113.xg4ken.com/pixel/v1?track=1&token=f15b3992-46df-44e1-a2f9-a3191e71b7bc&conversionType=conv&revenue=0.0&currency=USD&orderId=&promoCode=&customParam1=&customParam2=" width="1" height="1" />
+</noscript>
+
 </body>
 </html>
